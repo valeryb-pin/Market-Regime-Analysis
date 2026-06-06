a2c5bc56ed4b02c729fbe8b5bab50d25df7129d1
valeryb-pin <82976412+valeryb-pin@users.noreply.github.com>
2026-06-06 14:37:31 -0400

    Create features_selector.py

---
+++ src/preprocessing/features_selector.py
@@ -0,0 +1,17 @@
+FEATURE_COLUMNS = [
+
+    "rolling_imbalance",
+
+    "efficiency_ratio",
+
+    "compression",
+
+    "amihud",
+
+    "persistence",
+
+    "volume_range_ratio",
+
+    "volatility"
+
+]
